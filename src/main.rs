de4dd39c613dcc133a3a9925d4de82d7950ad826
Kariyania, Sahil <sahil.kariyania@bik.ai>
2023-10-04 01:20:29 +0530

    type optimised

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,10 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    let guess: u8 = guess.trim().parse().expect("Please type number!");
     println!("The secret number is : {secret_number}");
     println!("You guessed: {guess}");
-    match guess.cmp(&secret_number.to_string()) {
+    match guess.cmp(&secret_number) {
         Ordering::Less => println!("You guessed too small!"),
         Ordering::Equal => println!("Congratulations! You guessed correct."),
         Ordering::Greater => println!("You guessed too big!"),
