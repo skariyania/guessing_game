3706a311661fc1069952b396806ae1bff2ce0c25
Kariyania, Sahil <sahil.kariyania@bik.ai>
2023-10-04 01:27:06 +0530

    guess in loop

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,20 @@ use std::io;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(0..=100);
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u8 = guess.trim().parse().expect("Please type number!");
-    println!("The secret number is : {secret_number}");
-    println!("You guessed: {guess}");
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("You guessed too small!"),
-        Ordering::Equal => println!("Congratulations! You guessed correct."),
-        Ordering::Greater => println!("You guessed too big!"),
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u8 = guess.trim().parse().expect("Please type number!");
+        println!("The secret number is : {secret_number}");
+        println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("You guessed too small!"),
+            Ordering::Equal => println!("Congratulations! You guessed correct."),
+            Ordering::Greater => println!("You guessed too big!"),
+        }
     }
 }
