0a5491764caa97739860a942ca616c4e0abbb58d
Kariyania, Sahil <sahil.kariyania@bik.ai>
2023-10-04 00:55:02 +0530

    generating secret number

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input");
+    let secret_number = rand::thread_rng().gen_range(0..=100);
+    println!("Please input your guess.");
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    println!("The secret number is : {secret_number}");
     println!("You guessed: {guess}");
 }
